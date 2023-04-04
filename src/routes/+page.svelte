9380d7c079b66c8be72613ad8ba3904056d10818
benkovra <112646826+benkovra@users.noreply.github.com>
2023-04-04 17:51:36 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Title of Page change</h1>
+<p>Basic page description also change</p>
---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data;
+  </script>
+
+<h1>Title of Page</h1>
+<p>Basic page description</p>
+<aside>
+    <nav class="sidebar">
+        {#each data.summaries as {slug, title}}
+          <a href="/posts/{slug}">{title}</a>
+        {/each}
+    </nav>
+</aside>
